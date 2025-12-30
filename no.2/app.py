66076ca6491e477792d94add6903fbb219ac5e9e
zero1coding <mr.rout@gmail.com>
2025-12-30 16:17:25 +0530

    Add Flask project files

---
+++ no.2/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient(
+    "mongodb+srv://mrksrout_db_user:Mongo12345@cluster0.tlsegtu.mongodb.net/assignmentDB?retryWrites=true&w=majority"
+)
+
+db = client.assignmentDB
+collection = db.students
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    error = None
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            email = request.form['email']
+
+            collection.insert_one({
+                "name": name,
+                "email": email
+            })
+
+            return render_template('success.html')
+
+        except Exception as e:
+            error = str(e)
+
+    return render_template('form.html', error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
